455c2fd81a4f0334f42f7d2de111e8bc7196b0a2
zaikman <zachaikman@gmail.com>
2013-12-05 10:21:08 -0800

    Attempting to fix formatting issue with InspectorButton script.

--- InspectorButton.cs
+++ InspectorButton.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 public class InspectorButtonAttribute : PropertyAttribute
 {
   public static float kDefaultButtonWidth = 80;
-  
+
   public readonly string MethodName;
 
   private float _buttonWidth = kDefaultButtonWidth;
@@ -24,10 +24,10 @@ public class InspectorButtonAttribute : PropertyAttribute
     set { _buttonWidth = value; }
   }
 
-	public InspectorButtonAttribute(string MethodName)
-	{
-		this.MethodName = MethodName;
-	}
+  public InspectorButtonAttribute(string MethodName)
+  {
+    this.MethodName = MethodName;
+  }
 }
 
 #if UNITY_EDITOR
@@ -36,23 +36,23 @@ public class InspectorButtonPropertyDrawer : PropertyDrawer
 {
   private MethodInfo _eventMethodInfo = null;
 
-	public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
-	{
-	  InspectorButtonAttribute inspectorButtonAttribute = (InspectorButtonAttribute)attribute;
+  public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+  {
+    InspectorButtonAttribute inspectorButtonAttribute = (InspectorButtonAttribute)attribute;
     Rect buttonRect = new Rect(position.x + (position.width - inspectorButtonAttribute.ButtonWidth) * 0.5f, position.y, inspectorButtonAttribute.ButtonWidth, position.height);
-		if (GUI.Button(buttonRect, label.text))
-		{
-			System.Type eventOwnerType = prop.serializedObject.targetObject.GetType();
+    if (GUI.Button(buttonRect, label.text))
+    {
+      System.Type eventOwnerType = prop.serializedObject.targetObject.GetType();
       string eventName = inspectorButtonAttribute.MethodName;
-			
-      if (_eventMethodInfo == null)  
+
+      if (_eventMethodInfo == null)
         _eventMethodInfo = eventOwnerType.GetMethod(eventName, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
 
-		  if (_eventMethodInfo != null)
-		    _eventMethodInfo.Invoke(prop.serializedObject.targetObject, null);
-		  else
-		    Debug.LogWarning(string.Format("InspectorButton: Unable to find method {0} in {1}", eventName, eventOwnerType));
-		}
-	}
+      if (_eventMethodInfo != null)
+        _eventMethodInfo.Invoke(prop.serializedObject.targetObject, null);
+      else
+        Debug.LogWarning(string.Format("InspectorButton: Unable to find method {0} in {1}", eventName, eventOwnerType));
+    }
+  }
 }
 #endif
